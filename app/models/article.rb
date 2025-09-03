7a2c4f342d50bb1dc9ffadb219b837bb9bf4db70
br-to <barabara0224@gmail.com>
2025-09-04 00:09:21 +0900

    feat: Articleモデルにpublishedおよびvisibleスコープを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
+
+  scope :published, -> { where(published: true) }
+  scope :visible, -> { where(published: true).where("published_at IS NULL OR published_at <= ?", Time.current) }
 end
