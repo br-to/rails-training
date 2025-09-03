946bf9443b3175c87e2323ec8387094f0333fd8a
br-to <barabara0224@gmail.com>
2025-09-04 00:09:53 +0900

    feat: コメントテーブルを追加し、記事との関連付けを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_02_155118) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_02_162054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,4 +23,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_02_155118) do
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_articles_on_title", unique: true
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.text "body"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
