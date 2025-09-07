e0989113c79d12ba349236840bd12692fbc6ead9
br-to <barabara0224@gmail.com>
2025-09-07 18:32:28 +0900

    remove: logrageのカスタムオプションを削除

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -4,13 +4,6 @@ Rails.application.configure do
     config.lograge.base_controller_class = 'ActionController::API'
     config.lograge.formatter = Lograge::Formatters::Json.new
 
-    config.lograge.custom_options = lambda do |event|
-      {
-        request_id: event.payload[:request_id] || SecureRandom.uuid,
-        time: Time.now.iso8601
-      }
-    end
-
     config.lograge.custom_payload do |controller|
       {
         params: controller.request.filtered_parameters.except('controller', 'action')
