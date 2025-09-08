15179f132e0c45fe9513c4f955f316c926569af1
br-to <barabara0224@gmail.com>
2025-09-09 00:08:19 +0900

    feat: ApiTokenコントローラーとルーティングを追加し、トークンの生成と失効機能を実装

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -4,9 +4,29 @@ class ApiToken < ApplicationRecord
   scope :active, -> { where(revoked_at: nil) }
 
   def self.generate_for(user)
-    # tok_#{id}.#{secret} 形式のトークン生成
-    token = SecureRandom.hex(16)
-    create!(token: "tok_#{user.id}.#{token}", user: user)
+    # token_#{id}.#{secret} 形式のトークン生成
+    secret = SecureRandom.hex(32)
+    digest = Digest::SHA256.hexdigest(secret)
+
+    # レコード作成
+    api_token = create!(user: user, token_digest: digest)
+
+    # 作成後にplain_tokenを組み立て
+    plain_token = "token_#{api_token.id}.#{secret}"
+
+    plain_token
+  end
+
+  def self.find_by_token(plain_token)
+    # token_#{id}.#{secret} 形式のトークンからIDを抽出
+    match = plain_token.match(/\Atoken_(\d+)\.(.+)\z/)
+    return nil unless match
+
+    id = match[1].to_i
+    secret = match[2]
+
+    # IDとシークレットからトークンを検索
+    find_by(id: id, token_digest: Digest::SHA256.hexdigest(secret))
   end
 
   def revoke!
