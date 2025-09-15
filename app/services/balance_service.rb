12ad9bbbb9e53b94fad6006ac3bca60b3e13dd59
br-to <barabara0224@gmail.com>
2025-09-16 01:44:42 +0900

    feat: 転送メソッドにロック機能を追加し、同時実行性を向上

--- app/services/balance_service.rb
+++ app/services/balance_service.rb
@@ -42,4 +42,32 @@ class BalanceService
     Rails.logger.info("event=balance_update account_id=#{account_id} delta=-#{amount} result=error reason=#{e.class.name}")
     raise
   end
+
+  def self.transfer(from_account_id, to_account_id, amount)
+    # トランザクション内で実行
+    Account.transaction do
+      from_account = Account.lock.find(from_account_id)
+      to_account = Account.lock.find(to_account_id)
+
+      if from_account.balance < amount
+        raise InsufficientFundsError, "Insufficient funds in account #{from_account_id}"
+      end
+
+      from_account.balance -= amount
+      to_account.balance += amount
+
+      from_account.save!
+      to_account.save!
+
+      Rails.logger.info("event=balance_transfer from_account_id=#{from_account_id} to_account_id=#{to_account_id} amount=#{amount} result=ok")
+      true
+    end
+  rescue InsufficientFundsError => e
+    Rails.logger.info("event=balance_transfer from_account_id=#{from_account_id} to_account_id=#{to_account_id} amount=#{amount} result=error reason=insufficient_funds")
+    raise
+  rescue => e
+    # システムエラーログ
+    Rails.logger.info("event=balance_transfer from_account_id=#{from_account_id} to_account_id=#{to_account_id} amount=#{amount} result=error reason=#{e.class.name}")
+    raise
+  end
 end
